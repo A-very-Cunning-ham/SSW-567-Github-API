02b45bd83b6eabfae7c039a3e468970b3c7a3af2
A-very-Cunning-ham <ave210knife@gmail.com>
2021-03-17 00:17:52 -0400

    Remove old code

--- github.py
+++ github.py
@@ -1,18 +1,5 @@
 import requests
 
-# def listRepos(githubUserID):
-#     """Returns a list of repositories the respective number of commits for a given Github username"""
-#     r = requests.get(f"https://api.github.com/users/{githubUserID}/repos").json()
-#     results = []
-#     if not isinstance(r, list):
-#         raise TypeError(f"{r['message']}")
-#     for repo in r:
-#         name = repo.get("name")
-#         commits = len(requests.get(f"https://api.github.com/repos/{githubUserID}/{name}/commits").json())
-#         results.append([name, commits])
-#         print(f"Repo: {name} Number of commits: {commits}")
-#     return results
-
 def listRepos(githubUserID):
     """Returns a list of repositories the respective number of commits for a given Github username"""
     repos = getRepos(githubUserID)
